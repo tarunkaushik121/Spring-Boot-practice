ab3f4a15146e51aca9406c5dcb9d7ca37bd83472
tarunkaushik121 <tarunkaushik121@gmail.com>
2024-11-20 14:48:19 +0530

    Docker integration

--- src/main/java/com/practicespringboot/Pratice/EmployeeService.java
+++ src/main/java/com/practicespringboot/Pratice/EmployeeService.java
@@ -13,26 +13,28 @@ import java.util.stream.Collectors;
 public class EmployeeService {
 
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-
-        EmployeeService service=new EmployeeService();
-        service.sendReminderToEmployee().get();
-    }
+//    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//
+//        EmployeeService service=new EmployeeService();
+//        service.sendReminderToEmployee().get();
+//    }
 
     private CompletableFuture<Void> sendReminderToEmployee() {
-        Executor executor= Executors.newFixedThreadPool(5);
-        CompletableFuture<Void> mymethod=CompletableFuture.supplyAsync(()->{
-               return EmployeeDatabase.fetchEmployee();
-            },executor).thenApplyAsync((employees) -> {
-                return employees.stream().filter(employee -> "TRUE".equals(employee.getNewJoiner())).collect(Collectors.toList());
-                },executor).thenApplyAsync((employees)->{
+        Executor executor = Executors.newFixedThreadPool(5);
+        CompletableFuture<Void> mymethod = CompletableFuture.supplyAsync(() -> {
+                    return EmployeeDatabase.fetchEmployee();
+                }, executor)
+                .thenApplyAsync((employees) -> {
+                    return employees.stream().filter(employee -> "TRUE".equals(employee.getNewJoiner())).collect(Collectors.toList());
+                }, executor).thenApplyAsync((employees) -> {
                     return employees.stream().filter(employee -> "TRUE".equals(employee.getLearningPending())).collect(Collectors.toList());
-                },executor).thenApplyAsync((employees) -> {
-            System.out.println("get emails  : " + Thread.currentThread().getName());
-            return employees.stream().map(Employee::getEmail).collect(Collectors.toList());
-        },executor).thenAcceptAsync((emails)->{
-            emails.forEach(EmployeeService::sendEmail);
-        },executor);
+                }, executor)
+                .thenApplyAsync((employees) -> {
+                    return employees.stream().map(Employee::getEmail).collect(Collectors.toList());
+                }, executor)
+                .thenAcceptAsync((emails) -> {
+                    emails.forEach(EmployeeService::sendEmail);
+                }, executor);
         return mymethod;
 
     }
--- src/main/java/com/practicespringboot/Pratice/PraticeApplication.java
+++ src/main/java/com/practicespringboot/Pratice/PraticeApplication.java
@@ -2,9 +2,17 @@ package com.practicespringboot.Pratice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PraticeApplication {
+
+	@GetMapping("/test")
+	public String getMessage(){
+		return "Hello Tarun!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PraticeApplication.class, args);
